1a4ea5279898157c116582213c498e5811d31502
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 13:56:33 +0530

    Daily wage Calculation

--- src/com/employee/MainClass.java
+++ src/com/employee/MainClass.java
@@ -5,12 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		double attendance = Math.floor( (Math.random() * 10) % 2);
-		
+		int wage;
 		if (attendance == 0) {
 			System.out.println("Employee is absent");
 		}
 		else {
+			wage = 160;
 			System.out.println("Employess is present");
+			System.out.println("The wages earned = " + wage);
 		}
 	}
 
