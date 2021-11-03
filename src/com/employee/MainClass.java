87914edad57d034048d0f077d647ddffc1879810
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 13:36:35 +0530

    Checking Attendance

--- src/com/employee/MainClass.java
+++ src/com/employee/MainClass.java
@@ -3,8 +3,15 @@ package com.employee;
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		
+		double attendance = Math.floor( (Math.random() * 10) % 2);
+		
+		if (attendance == 0) {
+			System.out.println("Employee is absent");
+		}
+		else {
+			System.out.println("Employess is present");
+		}
 	}
 
 }
