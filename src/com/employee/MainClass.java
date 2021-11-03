82614b0719eb95035ee70324e355d2164d922284
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 14:15:39 +0530

    Adding Part Time Wages

--- src/com/employee/MainClass.java
+++ src/com/employee/MainClass.java
@@ -5,12 +5,18 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		double attendance = Math.floor( (Math.random() * 10) % 2);
+		double partTime = Math.floor( (Math.random() * 10) % 2);
 		int wage;
 		if (attendance == 0) {
 			System.out.println("Employee is absent");
 		}
 		else {
-			wage = 160;
+			if (partTime != 0) {
+				wage = 160;
+			}
+			else {
+				wage = 160;
+			}
 			System.out.println("Employess is present");
 			System.out.println("The wages earned = " + wage);
 		}
