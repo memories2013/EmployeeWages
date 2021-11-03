6ebe9552e30e0c6204609e053a13373359dba426
memories2013 <nshivaprasad41@gmail.com>
2021-11-03 14:23:52 +0530

    solving Switch Case Statements

--- src/com/employee/MainClass.java
+++ src/com/employee/MainClass.java
@@ -3,22 +3,26 @@ package com.employee;
 public class MainClass {
 
 	public static void main(String[] args) {
-		
-		double attendance = Math.floor( (Math.random() * 10) % 2);
+
+		int attendance = (int)Math.floor( (Math.random() * 10) % 2);
 		double partTime = Math.floor( (Math.random() * 10) % 2);
 		int wage;
-		if (attendance == 0) {
-			System.out.println("Employee is absent");
-		}
-		else {
-			if (partTime != 0) {
-				wage = 160;
-			}
-			else {
-				wage = 160;
-			}
-			System.out.println("Employess is present");
-			System.out.println("The wages earned = " + wage);
+		switch (attendance) {
+			case 0:
+				System.out.println("Employee is absent");
+				break;
+				
+			case 1: 
+				if (partTime != 0) {
+					wage = 160;
+				}
+				else {
+					wage = 160;
+				}
+				System.out.println("Employess is present");
+				System.out.println("The wages earned = " + wage);
+				break;
+			
 		}
 	}
 
